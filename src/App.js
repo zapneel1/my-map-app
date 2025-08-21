f3205bbaefb286c7e75e3ef702e45448cfede482
zapneel0 <70516833+zapneel0@users.noreply.github.com>
2025-08-22 01:32:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import Map from './components/map.js';
+//import Navbar from './components/navbar.js';
 import './App.css';
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Navbar/> */}
+      <Map/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/map.js
@@ -0,0 +1,41 @@
+import React, { useRef, useEffect, useState } from 'react';
+import maplibregl from 'maplibre-gl';
+import { GeocodingControl } from "@maptiler/geocoding-control/react";
+import { createMapLibreGlMapController } from "@maptiler/geocoding-control/maplibregl-controller";
+import "@maptiler/geocoding-control/style.css";
+import 'maplibre-gl/dist/maplibre-gl.css';
+import './map.css';
+
+export default function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const lat = 22.318257299063653;
+  const lng = 87.30583186511298;
+  const zoom = 16;
+  const API_KEY = 'AGMBRAsSD2L65HSvLA4i';
+  const [mapController, setMapController] = useState();
+
+  useEffect(() => {
+    if (map.current) return; // stops map from intializing more than once
+
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=${API_KEY}`,
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+    map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
+    setMapController(createMapLibreGlMapController(map.current, maplibregl));
+
+  }, [API_KEY, lng, lat, zoom]);
+
+  return (
+    <div className="map-wrap">
+      <div className="geocoding">
+        <GeocodingControl apiKey={API_KEY} mapController={mapController} />
+      </div>
+      <div ref={mapContainer} className="map" />
+    </div>
+  );
+}
